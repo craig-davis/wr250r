44a1395c2f21fe56ce9e47ebd5d9be77d8b4da42
Shane Reustle <me@shanereustle.com>
2015-02-21 16:26:52 +0700

    doc page design

--- wr250r/lib/router.js
+++ wr250r/lib/router.js
@@ -5,7 +5,19 @@ Router.route('/', {name : 'index'}, function(){
 	return this.render('index');
 });
 
-Router.route('/docs', {name : 'docsIndex', routeGroup : 'docs'}, function(){
+Router.route('/docs', function(){
+	
 	return this.render('docsIndex');
+	
+});
+
+Router.route('/docs/:docName', function(){
+	
+	var docName = this.params.docName;
+	
+	this.layout('docBase');
+	
+	return this.render('doc-' + docName);
+	
 });
 
