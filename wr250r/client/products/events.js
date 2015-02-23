7ccd13c0c2ad3e40a908cf616a5bcac9266dd01a
Shane Reustle <me@shanereustle.com>
2015-02-23 13:21:56 +0700

    much cleaner fancybox

--- wr250r/client/products/events.js
+++ wr250r/client/products/events.js
@@ -1,31 +1,12 @@
 Template.product.events({
-	
-	'click .image-thumb' : function(e){
+	'click a[rel=productImage]' : function(e){
 		e.preventDefault();
-		
-		var clickedLink = $(e.target).attr('data-image');
-		
-		// Grab this product
-		var thisProduct = Products.findOne({ url : Router.current().params.product });
-		
-		// Create a reordered image list, started with the clicked image
-		var clickedImageIndex = _.indexOf(thisProduct.images, clickedLink);
-		var sortedImageList = newArrayFront(thisProduct.images, clickedImageIndex);
-		
-		// Build images list for this product
-		var imageList = [];
-		_.each(sortedImageList, function(imageUrl){
-			imageList.push({
-				href : '/images/products/' + imageUrl
-			});
-		});
-		
-		// Init fancybox
-		$.fancybox.open(imageList, {
-			padding : 0
-		});
-		
 	}
-	
 });
+
+Template.product.rendered = function(){
+	$('a[rel=productImage]').fancybox({
+		padding : 0
+	});
+};
 
