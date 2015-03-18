8aafc91ff30dd892a4efbfd0398e59026334e585
Shane Reustle <me@shanereustle.com>
2015-03-18 09:23:53 +0700

    settingsjson field for static content

--- wr250r/client/spacebars-helpers.js
+++ wr250r/client/spacebars-helpers.js
@@ -1,6 +1,11 @@
 
 // {{#if equals foo '10' }}
-UI.registerHelper('equals', function (a, b) {
+UI.registerHelper('equals', function (a, b){
 	return a === b;
+});
+
+// {{ staticContentUrl }}
+UI.registerHelper('staticContentUrl', function (){
+	return Meteor.settings.public.staticContentUrl;
 });
 
